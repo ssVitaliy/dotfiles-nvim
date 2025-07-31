575dfc41a8a79b3d964b79da8e12ff5771352603
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-07-31 22:25:32 +0300

    add: Lsp 'lua_ls'
    Add lsp conf for Lua. Config blink to work with it.

--- lua/plugins/autocompletion--blink-cmp.lua
+++ lua/plugins/autocompletion--blink-cmp.lua
@@ -41,8 +41,16 @@ return {
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
 				default = { "lsp", "path", "snippets", "buffer" },
-			},
 
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						-- make lazydev completions top priority (see `:h blink.cmp`)
+						score_offset = 100,
+					},
+				},
+			},
 			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
 			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
 			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
