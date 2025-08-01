95f058ecbb59ab58f7ef928dd3956b4998e75491
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-08-02 00:55:45 +0300

    More keys and opts
    Opts
    -----------------------
    lang: en
    encoding: utf-8
    shell flag: -c
    -- Basic settings
    -- Indentation
    -- Search settings
    -- netrw: no banner, tree-style, width 30

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,10 +3,10 @@ vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("n", "<leader><Space>", "<C-w>w", { desc = "Cycle windows" })
 vim.keymap.set("n", "<leader>pp", '"*p', { desc = "Paste from common clipboard" })
 
-vim.keymap.set("n", "<leader>t", ":leftabove vertical 30split | Exp<CR>", { desc = "Left vert Explorer" })
+vim.keymap.set("n", "<leader>t", ":30Vex!<CR>", { desc = "Left vert Explorer" })
 
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>") -- Turn off search highlight
 
-vim.keymap.set("n", "<leader>el", ":.lua<CR>", { desc = "Eval one line in lua" })
-vim.keymap.set("n", "<leader>eb", ":%lua<CR>", { desc = "Eval entire buffer" })
+vim.keymap.set("n", "<leader>rl", ":.lua<CR>", { desc = "Eval one line in lua" })
+vim.keymap.set("n", "<leader>rb", ":%lua<CR>", { desc = "Eval entire buffer" })
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Write current file" })
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,19 +1,46 @@
--- Set <leader>
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.cmd([[language en_US.UTF-8]])
 
-vim.o.autochdir = true -- Auto change dir
-vim.o.number = true -- line numbers default
--- vim.o.relativenumber = true  -- relative line numbers
+-- Set encoding for file content
+vim.opt.encoding = "UTF-8"
+vim.opt.fileencoding = "utf-8"
 
-vim.opt.tabstop = 4 -- Number of spaces a TAB counts for
-vim.opt.shiftwidth = 4 -- Number of spaces to use for autoindent
-vim.opt.softtabstop = 4 -- Number of spaces a TAB counts for while editing
-vim.opt.expandtab = true -- Convert tabs to spaces (recommended for Python)
+vim.o.shellcmdflag = "-c" -- Set up shell
 
-vim.o.mouse = "a" -- Enable mouse mode, can be useful for resizing splits for example!
+vim.g.mapleader = " " -- Set global <leader>
+vim.g.maplocalleader = " " -- Set local <leader>
 
--- vim.o.showmode = false  -- Don't show the mode, since it's already in the status line
+vim.cmd.colorscheme("habamax")
+
+-- Basic settings
+vim.opt.number = true -- Line numbers
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.cursorline = true -- Highlight current line
+vim.opt.wrap = false -- Don't wrap lines
+vim.opt.scrolloff = 8 -- Keep 10 lines above/below cursor
+vim.opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
+
+-- Indentation
+vim.opt.tabstop = 2 -- Tab width
+vim.opt.shiftwidth = 2 -- Indent width
+vim.opt.softtabstop = 2 -- Soft tab stop
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Smart auto-indenting
+vim.opt.autoindent = true -- Copy indent from current line
+
+-- Search settings
+vim.opt.ignorecase = true -- Case insensitive search
+vim.opt.smartcase = true -- Case sensitive if uppercase in search
+vim.opt.hlsearch = true -- highlight search results
+vim.opt.incsearch = true -- Show matches as you type
+
+vim.o.autochdir = false -- Auto change dir
+
+vim.o.mouse = "a" -- Enable mouse mode
+
+-- netrw
+vim.g.netrw_banner = 0 -- turn off the banner in Netrw
+vim.g.netrw_liststyle = 3 -- Tree-style listing
+-- vim.g.netrw_winsize = 30 -- Window width when opening in vertical split
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
@@ -25,9 +52,6 @@ end)
 vim.o.breakindent = true -- Enable break indent
 
 -- vim.o.undofile = true  -- Save undo history
-
-vim.o.ignorecase = true -- Case-insensitive searching
-vim.o.smartcase = true --
 
 vim.o.signcolumn = "yes" -- Keep signcolumn on by default
 
@@ -50,8 +74,6 @@ vim.o.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 vim.o.inccommand = "split" -- Preview substitutions live, as you type!
-
-vim.o.scrolloff = 8 -- Minimal number of screen lines to keep above and below the cursor.
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
