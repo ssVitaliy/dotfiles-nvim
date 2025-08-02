5bcb5b16bc47e5d9bd1a47a0b87cfbce3ecfdd30
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-08-02 06:36:25 +0300

    Turn on search highlight

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -70,7 +70,7 @@ vim.o.splitbelow = true
 --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
 --   See `:help lua-options`
 --   and `:help lua-options-guide`
-vim.o.list = true
+vim.o.list = false
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 vim.o.inccommand = "split" -- Preview substitutions live, as you type!
