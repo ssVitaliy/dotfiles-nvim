f3fcb671e7914e4cea74c95f119e3e5ffdb635d3
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-08-04 06:37:13 +0300

    chore: keymap main actions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,11 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>of", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>og", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>oo", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>oh", builtin.help_tags, { desc = "Telescope help tags" })
+	end,
 }
