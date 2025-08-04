a99d6e893088eefb26bdcd6b2570e2808cc16af2
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-08-04 06:03:55 +0300

    feat: Bind 'zz' in the status section to 'stash staged with message'

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -7,7 +7,25 @@ return {
 		},
 		config = function()
 			local neogit = require("neogit")
-			neogit.setup({})
+			neogit.setup({
+				mappings = {
+					status = {
+						-- Bind 'zz' in the status section to stash staged with message
+						["zz"] = function()
+							-- Prompt for stash name
+							local stash_name = vim.fn.input("Stash name: ")
+							if stash_name == "" then
+								print("No stash name provided, aborted.")
+								return
+							end
+
+							-- Execute: stash ONLY staged changes with the provided name
+							vim.cmd("!git stash push --staged -m " .. stash_name)
+							vim.notify("Staged changes stashed as: " .. stash_name, vim.log.levels.INFO)
+						end,
+					},
+				},
+			})
 		end,
 	},
 }
