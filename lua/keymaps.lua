e3ba17dab1023505ed246cb82029c6561f01318f
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-08-05 03:32:22 +0300

    chore: keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,12 +1,24 @@
---- Use 'jk' to exit from INSERT mode
-vim.keymap.set("i", "jk", "<ESC>")
-vim.keymap.set("n", "<leader><Space>", "<C-w>w", { desc = "Cycle windows" })
+-- hard coded keys
+vim.keymap.set("i", "jk", "<ESC>") -- Use 'jk' to exit from INSERT mode
 vim.keymap.set("n", "<leader>pp", '"*p', { desc = "Paste from common clipboard" })
-
-vim.keymap.set("n", "<leader>t", ":30Vex!<CR>", { desc = "Left vert Explorer" })
-
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>") -- Turn off search highlight
 
-vim.keymap.set("n", "<leader>rl", ":.lua<CR>", { desc = "Eval one line in lua" })
-vim.keymap.set("n", "<leader>rb", ":%lua<CR>", { desc = "Eval entire buffer" })
+-- middle coded
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Write current file" })
+vim.keymap.set("n", "<leader>ad", ":DiffviewOpen<CR>", { desc = "Diffview Open" })
+
+-- Yank to / paste from register 't'
+vim.keymap.set({ "n", "v" }, "<leader>y", '"*y', { desc = "Yank to clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"*p', { desc = "Paste to clipboard" })
+
+-- temporary
+vim.keymap.set("n", "<leader>t", ":tab split<CR>", { desc = "Open in new tab" })
+vim.keymap.set("n", "<leader>tt", ":tabc<CR>", { desc = "Close tab" })
+
+vim.keymap.set("n", "<leader>el", ":.lua<CR>", { desc = "Eval one line in lua" })
+vim.keymap.set("n", "<leader>ea", ":%lua<CR>", { desc = "Eval entire buffer" })
+
+-- vim.keymap.set("n", "<leader>t", ":30Vex!<CR>", { desc = "Left side Explorer" })
+vim.keymap.set("n", "<leader>`", ":Neogit<CR>", { desc = "open neoGit" })
+
+vim.keymap.set("n", "<leader>m", vim.diagnostic.open_float, { desc = "Write current file" })
