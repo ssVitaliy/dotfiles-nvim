6c01a9dd7453097604283de6fbd9ac34a8c0041b
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-08-10 12:33:32 +0300

    chore(keys): move lines, reselect after indent

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,3 +31,13 @@ vim.keymap.set("n", "<leader>ea", ":%lua<CR>", { desc = "Eval entire buffer" })
 vim.keymap.set("n", "<leader>`", ":Neogit<CR>", { desc = "open neoGit" })
 
 vim.keymap.set("n", "<leader>'", vim.diagnostic.open_float, { desc = "Diag message" })
+
+-- Move lines up/down
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Better indenting in visual mode
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
