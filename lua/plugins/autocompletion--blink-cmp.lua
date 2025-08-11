0e333750d5daff6ffa1b6734db38e2e62e345340
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-08-11 03:43:27 +0300

    fix(blink): integration with LazyDev
    Install Lazydev, update 'lock'.
    Add provider to Blink sources.

--- lua/plugins/autocompletion--blink-cmp.lua
+++ lua/plugins/autocompletion--blink-cmp.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"saghen/blink.cmp",
 		-- optional: provides snippets for the snippet source
-		dependencies = { "rafamadriz/friendly-snippets" },
+		dependencies = { "rafamadriz/friendly-snippets", "folke/lazydev.nvim" },
 
 		-- use a release tag to download pre-built binaries
 		version = "1.*",
@@ -34,7 +34,7 @@ return {
 			-- Default list of enabled providers defined so that you can extend it
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
+				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
 
 				providers = {
 					lazydev = {
