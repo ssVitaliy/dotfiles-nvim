27c13811bb40fe286b7632af33b0bd3502d2f17b
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-08-15 22:18:53 +0300

    feat: Smart-side vertical compact layout
    Vertical_compact strategie: merge prompt and result windows.
    Funcs is_right_side and adjust_anchor to determine where telescope
    will appears.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,43 @@ return {
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		require("telescope").setup({
 
+		---=== Custom layout_strategie: merge prompt and result windows
+		--stylua: ignore
+		require("telescope.pickers.layout_strategies").vertical_compact = function(
+			picker, max_columns, max_lines, layout_config)
+
+			local layout =
+				require("telescope.pickers.layout_strategies").vertical(picker, max_columns, max_lines, layout_config)
+			layout.results.line = layout.results.line - 1
+			layout.results.height = layout.results.height + 1
+			return layout
+		end
+
+		---=== Custom funcs ===---
+		-- if window x pos > 40% of width return true
+		local function is_right_side()
+			local win_id = vim.api.nvim_get_current_win()
+			local win_pos_x = vim.api.nvim_win_get_position(win_id)[2]
+			local total_width = vim.opt.columns:get()
+
+			if win_pos_x > total_width * 0.4 then
+				return true
+			else
+				return false
+			end
+		end
+
+		local function adjust_anchor(view)
+			if is_right_side() then
+				view.layout_config.anchor = "W"
+			else
+				view.layout_config.anchor = "E"
+			end
+			return view
+		end
+
+		require("telescope").setup({
 			defaults = {
 				vimgrep_arguments = {
 					"rg",
@@ -38,22 +73,93 @@ return {
 		end
 
 		--=== Themes ===--
-		local themes = require("telescope.themes")
-
-		-- Dropdown theme (80% width)
-		local common_vert = themes.get_dropdown({
-			layout_strategy = "vertical",
+		local vert_comp = {
+			layout_strategy = "vertical_compact",
+			sorting_strategy = "ascending",
+			results_title = false,
 			layout_config = {
-				-- width = 0.7,
+				prompt_position = "top",
+				anchor = "E",
+				preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+
 				width = function(_, max_columns, _)
 					return math.min(max_columns, 88)
 				end,
-				height = 0.95,
-				preview_height = 0.6,
-				prompt_position = "top",
-				anchor = "E",
+
+				height = function(_, _, max_lines)
+					return max_lines
+				end,
 			},
-		})
+		}
+
+		--=== Keys
+		local builtin = require("telescope.builtin")
+
+		-- Find files
+		vim.keymap.set("n", "<leader>oo", function()
+			builtin.find_files(adjust_anchor(vert_comp))
+		end, { desc = "Find files" })
+
+		-- Live grep
+		vim.keymap.set("n", "<leader>og", function()
+			builtin.live_grep(adjust_anchor(vert_comp))
+		end, { desc = "Live grep" })
+
+		-- Buffers
+		vim.keymap.set("n", "<leader>ob", function()
+			builtin.buffers(adjust_anchor(vert_comp))
+		end, { desc = "Find buffers" })
+
+		-- Find help tags
+		vim.keymap.set("n", "<leader>oh", function()
+			builtin.help_tags(adjust_anchor(vert_comp))
+		end, { desc = "Find help" })
+
+		-- vim.keymap.set("n", "<leader>of", builtin.find_files, { desc = "Telescope find files" })
+		-- vim.keymap.set("n", "<leader>og", builtin.live_grep, { desc = "Telescope live grep" })
+		-- vim.keymap.set("n", "<leader>oo", builtin.buffers, { desc = "Telescope buffers" })
+		-- vim.keymap.set("n", "<leader>oh", builtin.help_tags, { desc = "Telescope help tags" })
+	end,
+}
+--[[
+		--=== Themes ===--
+		local themes = require("telescope.themes")
+
+		local common_vert = {
+
+			results_title = false,
+			sorting_strategy = "ascending",
+			layout_strategy = "vertical_compact",
+			layout_config = {
+				prompt_position = "top",
+
+				anchor = "E",
+				preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+
+				width = function(_, max_columns, _)
+					return math.min(max_columns, 88)
+				end,
+
+				height = function(_, _, max_lines)
+					return math.min(max_lines, 48)
+				end,
+			},
+		}
+
+		-- -- Dropdown theme (80% width)
+		-- local common_vert = themes.get_dropdown({
+		-- 	layout_strategy = "vertical",
+		-- 	layout_config = {
+		-- 		-- width = 0.7,
+		-- 		width = function(_, max_columns, _)
+		-- 			return math.min(max_columns, 88)
+		-- 		end,
+		-- 		height = 0.95,
+		-- 		preview_height = 0.6,
+		-- 		prompt_position = "top",
+		-- 		anchor = "E",
+		-- 	},
+		-- })
 
 		-- Vertical theme (narrow sidebar)
 		local my_vertical = themes.get_ivy({
@@ -74,32 +180,4 @@ return {
 			},
 		})
 
-		--=== Keys
-		local builtin = require("telescope.builtin")
-
-		-- Find files
-		vim.keymap.set("n", "<leader>oo", function()
-			builtin.find_files(common_vert)
-		end, { desc = "Find files" })
-
-		-- Live grep
-		vim.keymap.set("n", "<leader>og", function()
-			builtin.live_grep(common_vert)
-		end, { desc = "Live grep" })
-
-		-- Buffers
-		vim.keymap.set("n", "<leader>ob", function()
-			builtin.buffers(common_vert)
-		end, { desc = "Find buffers" })
-
-		-- Find help tags
-		vim.keymap.set("n", "<leader>oh", function()
-			builtin.help_tags(common_vert)
-		end, { desc = "Find help" })
-
-		-- vim.keymap.set("n", "<leader>of", builtin.find_files, { desc = "Telescope find files" })
-		-- vim.keymap.set("n", "<leader>og", builtin.live_grep, { desc = "Telescope live grep" })
-		-- vim.keymap.set("n", "<leader>oo", builtin.buffers, { desc = "Telescope buffers" })
-		-- vim.keymap.set("n", "<leader>oh", builtin.help_tags, { desc = "Telescope help tags" })
-	end,
-}
+--]]
