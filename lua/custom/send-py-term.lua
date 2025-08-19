5932438e36d211aa23378695420ae502b12ae21c
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-08-20 02:23:17 +0300

    feat: Send block with corrected indentation
    Breaking changes:
    rename func term_send_line -> term_send_one_line
    
    Feature:
    Send block of code (selected or bounded) with correct indents.
    Determine the indentation from the first non-blank line.

--- lua/custom/send-py-term.lua
+++ lua/custom/send-py-term.lua
@@ -1,19 +1,17 @@
 local M = {}
 
---== Dev Stuff
--- vim.keymap.set("n", "<F9>", function()
--- 	package.loaded["funcs.tmp.spt"] = nil
--- 	require("funcs.tmp.spt").send_bounded()
--- end)
-
 -- package.loaded['funcs.tmp.spt'] = nil
 -- vim.fn.systemlist('wezterm cli send-text --pane-id 1 --no-paste "n\'i"')
 
-local pane_id = 1
+M.config = {}
+M.config.pane_id = 1
 local buff_id = 0
 local anchor = "#-"
 
 -- NICE but need in optimization
+-- Now we get whole buffer. This method not optimal, if buffer have many lines (>200).
+-- Much better to store bounds, on each call check changes, update if necessary.
+-- Also we can load buffer by parts, by 100 lines for example.
 local function get_bounded_lines()
 	local lines = vim.api.nvim_buf_get_lines(buff_id, 0, -1, false)
 	local start_mark = 1
@@ -41,16 +39,62 @@ local function escape_line(line)
 	return line:gsub("'", "'\\''")
 end
 
--- GOOD
-local function term_send_line(line)
+local function prepare_lines(lines)
+	-- Determine indent by the first non-blank line, unindent whole block
+	local result_lines = {}
+	local effective_line_pos = nil
+	local block_start = 1
+
+	for n, line in ipairs(lines) do
+		if line:sub(1, 1) ~= "#" then
+			local finder = line:find("[^%s]")
+			if finder then
+				effective_line_pos = finder
+				block_start = n
+				break
+			end
+		end
+	end
+
+	if not effective_line_pos then
+		return nil
+	end
+
+	for i = block_start, #lines do
+		local line = lines[i]
+		-- break on lines with something on indent area
+		if line:sub(1, effective_line_pos - 1):find("[^%s]") then
+			break
+		else
+			table.insert(result_lines, line:sub(effective_line_pos, -1))
+		end
+	end
+
+	-- if last line not empty then add <CR> (empty line) to eval block
+	if result_lines[#result_lines]:match("^%s") then
+		table.insert(result_lines, "")
+	end
+
+	return result_lines
+end
+
+local function term_send_one_line(line)
 	-- stylua: ignore
 	local cli_prefix = string.format(
-		"wezterm cli send-text --pane-id %s --no-paste ", pane_id)
-	local cli_enter = cli_prefix .. "$'\\r'"
-	local cmd = cli_prefix .. "'" .. escape_line(line) .. "'"
+		"wezterm cli send-text --pane-id %s --no-paste ", M.config.pane_id)
+	local cmd_enter = cli_prefix .. "$'\\r'"
 
+	local cmd = cli_prefix .. "'" .. escape_line(line) .. "'"
+	-- system call: send to term
 	vim.fn.systemlist(cmd)
-	vim.fn.systemlist(cli_enter)
+	vim.fn.systemlist(cmd_enter)
+	-- print(cmd)
+end
+
+local function term_send_lines(lines)
+	for _, line in ipairs(lines) do
+		term_send_one_line(line)
+	end
 end
 
 -- GOOD
@@ -64,26 +108,33 @@ end
 -- GOOD
 function M.send_selected()
 	local lines = get_selected_lines()
-	for _, line in ipairs(lines) do
-		term_send_line(line)
-	end
+	lines = prepare_lines(lines)
+	term_send_lines(lines)
 end
 
 -- GOOD
 function M.send_line()
 	local line = vim.api.nvim_get_current_line()
-	term_send_line(line)
+	term_send_one_line(line)
+	print("line sended")
 end
 
 function M.send_bounded()
-	for _, line in ipairs(get_bounded_lines()) do
-		-- print(line)
-		term_send_line(line)
+	-- term_send_lines(prepare_lines(get_bounded_lines()))
+
+	local bounds = get_bounded_lines()
+	for _, line in ipairs(bounds) do
+		print(line)
 	end
+	print("len bounds:", #bounds)
+	local prepared = prepare_lines(bounds)
+	print(prepared)
+
+	term_send_lines(prepared)
 end
 
 function M.send_whole_file()
-	term_send_line("python " .. vim.fn.expand("%:p"))
+	term_send_one_line("python " .. vim.fn.expand("%:p"))
 end
 
 function M.setup()
