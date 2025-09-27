f7aa8dad209ad3ca61c31070651ca9037e91d1a7
Vitaliy Ushakov <vvs.2552@yandex.ru>
2025-09-27 19:44:21 +0300

    fix(options): Smartindent false

--- lua/options.lua
+++ lua/options.lua
@@ -29,7 +29,7 @@ vim.opt.tabstop = 2 -- Tab width
 vim.opt.shiftwidth = 2 -- Indent width
 vim.opt.softtabstop = 2 -- Soft tab stop
 vim.opt.expandtab = false -- Use spaces instead of tabs
-vim.opt.smartindent = true -- Smart auto-indenting
+vim.opt.smartindent = false -- Smart auto-indenting
 vim.opt.autoindent = true -- Copy indent from current line
 
 -- Folds
